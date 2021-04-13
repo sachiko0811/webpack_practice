b157c659beccc391503f14a6119cd7fd047242e3
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-04-13 15:48:57 -0700

    style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,13 @@ module.exports = {
                 test: /\.css/,
                 use: [
                     {
+                        loader: 'style-loader'
+                    },
+                    {
                         loader: 'css-loader'
-                    }
-                ]
-            }
-        ]
-    }
+                    },
+                ],
+            },
+        ],
+    },
 }
